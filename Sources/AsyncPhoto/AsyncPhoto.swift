e1210427c1bef5c2d28d80460e6c60d73a1f60d6
Alok Kumar <alok.kumar@ovyo.com>
2025-05-01 15:15:37 +0530

    saving changes: added image changes

--- Sources/AsyncPhoto/AsyncPhoto.swift
+++ Sources/AsyncPhoto/AsyncPhoto.swift
@@ -3,6 +3,12 @@
 
 import SwiftUI
 
+#if os(iOS)
+import UIKit
+#elseif os(macOS)
+import AppKit
+#endif
+
 public struct AsyncPhoto: View {
     
     @StateObject private var viewModel: AsyncViewModel
@@ -16,11 +22,7 @@ public struct AsyncPhoto: View {
     public var body: some View {
         Group {
             if let image = image {
-                
                 image
-                    .resizable()
-                    .scaledToFit()
-                    .frame(width: 200)
             } else {
                 ProgressView()
                     .onAppear {
@@ -33,12 +35,28 @@ public struct AsyncPhoto: View {
         }
     }
     
-    private func imageFromData(_ data: Data) -> Image {
-        if let cgImageSource = CGImageSourceCreateWithData(data as CFData, nil),
-           let cgImage = CGImageSourceCreateImageAtIndex(cgImageSource, 0, nil) {
-            return Image(decorative: cgImage, scale: 1.0, orientation: .up)
-        } else {
-            return Image(systemName: "photo")
+//    private func imageFromData(_ data: Data) -> some View {
+////        if let cgImageSource = CGImageSourceCreateWithData(data as CFData, nil),
+////           let cgImage = CGImageSourceCreateImageAtIndex(cgImageSource, 0, nil) {
+////            return Image(decorative: cgImage, scale: 1.0, orientation: .up)
+////        } else {
+////            return Image(systemName: "photo")
+////        }
+//        if let image = UIImage(data: data) {
+//            return Image(ima)
+//        }
+//    }
+    
+    func imageFromData(_ data: Data) -> Image? {
+        #if os(iOS)
+        if let uiImage = UIImage(data: data) {
+            return Image(uiImage: uiImage)
         }
+        #elseif os(macOS)
+        if let nsImage = NSImage(data: data) {
+            return Image(nsImage: nsImage)
+        }
+        #endif
+        return nil
     }
 }
